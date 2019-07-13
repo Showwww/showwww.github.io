99fd6b45ffd2f33a1ccd7859b1644d3a7d335f82
showww <ole>
2019-07-13 14:32:51 +0300

    od

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "../assets/scss/style.scss"
+import $ from "jquery"
+import "bootstrap"
+
+$(document).ready(function () {
+
+  function plus(a, b) {
+    return a + b;
+  }
+
+  const carouselOptions = {
+    interval: 10000,
+
+  };
+
+  $('.carousel').carousel(carouselOptions);
+
+});
